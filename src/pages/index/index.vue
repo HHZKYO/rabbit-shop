a1235fb97277d34597dc9eea5a8dac36bcf1a657
HHZKYO <1692189552@qq.com>
2025-06-20 18:46:38 +0800

    使用uni-ui组件库

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,18 @@
 </script>
 
 <template>
-  <view class="index">index</view>
+  <uni-badge text="1">1</uni-badge>
+  <uni-badge text="2" type="success">1</uni-badge>
+  <uni-badge text="3" type="primary" :inverted="true">1</uni-badge>
+  <uni-card
+    title="基础卡片"
+    sub-title="副标题"
+    extra="额外信息"
+    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+  >
+    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+  </uni-card>
+  <view class="index">index123</view>
 </template>
 
 <style lang="scss">
